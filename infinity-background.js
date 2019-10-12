0df20810ddab08d995e71e510da22623690acfbb
Samuel Goncalves <samuel.goncalves@snapper.it>
2019-10-12 02:51:59 -0700

    code improvements

--- infinity-background.js
+++ infinity-background.js
@@ -1,22 +1,109 @@
-const _infbg_ = {
+/*
+* Infinity Background
+* by @goonca (https://github.com/goonca)
+*/
 
-  //used to prevent user fast mouse movements
-  MAX_SPEED : 0.3,
-  //background movement delay in milliseconds
-  DELAY : 250,
-  SCALE : 0.05,
+window.$goonca = (window.$goonca || {});
 
-  apply : props => {
+$goonca.infinityBackground = function(props) {
+
+  const SCALE = 0.05; //coeficient to move the background (could be passed as a param)
+  let MAX_SPEED = 0.3; //used to prevent user fast mouse movements
+  let DELAY = 250; //background movement delay in milliseconds
+  let body; //floating background
+
+  //variables to store background measure
+  const measure = { _x : 0, _l : 0, _t : 0, _dl : 0, _dt : 0};
+
+  /**
+  * _updatePositions()
+  * Assign mouse x and y positions to the measure variable. 
+  * @param  e: mouse event
+  * @return undefined
+  */
+  const _updatePositions = e => {
+    measure._x = e.originalEvent.clientX;
+    measure._y = e.originalEvent.clientY;
+  }
+
+  /**
+  * _getPiexelsToMove()
+  * Calculate the number of piexels to move based on scale and speed
+  * @param  e: mouse event
+  * @param  position: string indicating the movement direction 
+  * @return number of piexels
+  */
+  const _getPiexelsToMove = (e, position) => {
+
+    const mapper = new Map([
+      ['left', {b:'_dl', c:'_l', d:'_x', e:'clientX'}],
+      ['top', {b:'_dt', c:'_t', d:'_y', e:'clientY'}]
+    ]).get(position);
+
+    let piexelsToMove;
+    measure[mapper.b] = measure[mapper.c] - body.offset()[position];
+    
+    return piexelsToMove = Math.min(
+      Math.abs((piexelsToMove = ((e.originalEvent[mapper.e] - measure[mapper.d]) * SCALE))),
+      MAX_SPEED
+    ) * ((piexelsToMove > 0) ? 1 : -1)
+  }
+
+  /**
+  * _mouseMove()
+  * mouse event to update left and top of the floating background
+  * see _getPiexelsToMove()
+  * @param  e: mouse event
+  * @return undefined
+  */
+  const _mouseMove = e => {
+
+    body.css(
+      'left',
+      body.offset().left - _getPiexelsToMove(e, 'left')
+    );
+
+    //top position needs to considerate body scroll top position
+    body.css(
+      'top',
+      body.offset().top - _getPiexelsToMove(e, 'top') - document.body.scrollTop
+    );
+
+    _updatePositions(e);
+  }
+
+  /**
+  * _mouseOver()
+  * store the floating background left and top position
+  * to be used to calculate its movement on _mouseMove
+  * see _getPiexelsToMove()
+  * @param  e: mouse event
+  * @return undefined
+  */
+  const _mouseOver = e => {
+
+    measure._l = body.offset().left;
+    measure._t = body.offset().top;
+    _updatePositions(e);
+  }
+
+  /**
+  * _apply()
+  * apply the infinity background into the target defined in the properties
+  * @return undefined
+  */
+  const _apply = () => {
 
     //overriding default properties
-    _infbg_.MAX_SPEED = props.maxSpeed || _infbg_.MAX_SPEED;
-    _infbg_.DELAY = props.delay || _infbg_.DELAY;
+    MAX_SPEED = props.maxSpeed || MAX_SPEED;
+    DELAY = props.delay || DELAY;
 
+    //store the target html content to be wrapped
     const contentHtml = props.target.html();
 
     //copying content to a new container
     props.target.html(
-      $('<div></div>')
+      $('<div/>')
         .html(contentHtml)
         .css({
           'position' : 'absolute',
@@ -27,7 +114,7 @@ const _infbg_ = {
         })
     ).prepend(
       //creating floating background
-      _infbg_.body = $('<div></div>').css({
+      body = $('<div/>').css({
         'background-image' : props.target.css('background-image'),
         'background-repeat': 'no-repeat',
         'background-position': 'center',
@@ -44,54 +131,17 @@ const _infbg_ = {
       'position' : 'relative'
     });
 
-    _infbg_._x = 0;
-    _infbg_._l = 0;
-    _infbg_._t = 0;
-    _infbg_._dl = 0;
-    _infbg_._dt = 0;
     props.target
-      .on('mousemove', e => (setTimeout(_infbg_.mouseMove.bind(this, e), _infbg_.DELAY)))
-      .on('mouseover', _infbg_.mouseOver);
-  },
+      .on('mousemove', e => (setTimeout(_mouseMove.bind(this, e), DELAY)))
+      .on('mouseover', _mouseOver);
+  }  
 
-  updatePositions : e => {
-    _infbg_._x = e.originalEvent.clientX;
-    _infbg_._y = e.originalEvent.clientY;
-  },
-
-  getPiexelsToMove : (event, position, add) => {
-
-    const mapper = new Map([
-      ['left', {b:'_dl', c:'_l', d:'_x', e:'clientX'}],
-      ['top', {b:'_dt', c:'_t', d:'_y', e:'clientY'}]
-    ]).get(position);
-
-    let piexelsToMove
-    _infbg_[mapper.b] = _infbg_[mapper.c] - _infbg_.body.offset()[position] + (add || 0);
-    
-    return piexelsToMove = Math.min(
-      Math.abs((piexelsToMove = ((event.originalEvent[mapper.e] - _infbg_[mapper.d])*_infbg_.SCALE))),
-      _infbg_.MAX_SPEED
-    ) * ((piexelsToMove > 0) ? 1 : -1)
-  },
-
-  mouseMove : e => {
-
-    _infbg_.body.css('left', _infbg_.body.offset().left - _infbg_.getPiexelsToMove(e, 'left'));
-    _infbg_.body.css('top', _infbg_.body.offset().top - _infbg_.getPiexelsToMove(e, 'top', document.body.scrollTop));
-
-    _infbg_.updatePositions(e);
-  },
-
-  mouseOver : e => {
-
-    _infbg_._l = _infbg_.body.offset().left;
-    _infbg_._t = _infbg_.body.offset().top;
-    _infbg_.updatePositions(e);
-  }
-};
+  //public methods
+  return {apply : _apply}
+}
 
 $(document).ready(() => {
 
-  $('[infinity-background]').each((i, obj) => (_infbg_.apply({target : $(obj)})))
+  $('.gonca-infinity-background')
+    .each((i, obj) => (new $goonca.infinityBackground({target : $(obj)}).apply()));
 });
